26ae1bd2bcfa48c8c1bd3f7c2145e43c75d5e67c
Jente Van de Velde <jn.07@hotmail.com>
2019-06-13 13:23:09 +0200

    Update NILMrun.py

--- NILMrun.py
+++ NILMrun.py
@@ -2,4 +2,11 @@ from NILM import *
 import numpy as np
 import math
 import warnings
-
+class NILM:
+  def __init__():
+    self.step=0
+    print("This algortihm will aid you in the setup of the NILM algortihm.")
+    print("The algortihm will give several existing options, answer using'setup=NILMselect(VAUE)'")
+    print("With VALUE being the integer (or sometimes other parameter) representing the selected option")
+    print("")
+    
